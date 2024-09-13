bdf9bb97ba165c6f82007c7b292ff43c56ad8606
xl <173978372@qq.com>
2024-09-13 22:04:17 +0800

    图片滤镜

--- src/views/children/Menu.vue
+++ src/views/children/Menu.vue
@@ -34,6 +34,6 @@ const emit = defineEmits(['click'])
 </script>
 <style scoped>
 .filter {
-  filter: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg'><filter id='colorize'><feColorMatrix type='matrix' values='1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0' /></filter></svg>#colorize");
+  filter: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg'><filter id='colorize'><feColorMatrix type='matrix' values='1 0 0 0 0.698 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0' /></filter></svg>#colorize");
 }
 </style>
